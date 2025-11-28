3192cf09b2abc64a6d5940b0db479fa5bd91ee0b
sleepyjirachi1 <tickethandler@outlook.com>
2025-11-28 07:31:27 +0000

    refactor: main plugin class

---
+++ src/main/java/org/Autumn/moreNexoMechanics/Main.java
@@ -0,0 +1,88 @@
+package org.Autumn.moreNexoMechanics;
+
+/* Factories */
+import org.Autumn.moreNexoMechanics.mechanics.factories.SwappableEnchantsFactory;
+import org.Autumn.moreNexoMechanics.mechanics.factories.PotionAurasFactory;
+import org.Autumn.moreNexoMechanics.mechanics.factories.StatTrackFactory;
+
+/* Listeners */
+import org.Autumn.moreNexoMechanics.mechanics.listeners.SwappableEnchantsListener;
+import org.Autumn.moreNexoMechanics.mechanics.listeners.StatTrackListener;
+
+/* Runnables */
+import org.Autumn.moreNexoMechanics.mechanics.runnables.PotionAurasTask;
+
+/* Nexo */
+import com.nexomc.nexo.api.events.NexoMechanicsRegisteredEvent;
+import com.nexomc.nexo.mechanics.MechanicsManager;
+import com.nexomc.nexo.utils.logs.Logs;
+
+/* Bukkit */
+import org.bukkit.event.Listener;
+import org.bukkit.Bukkit;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.PluginCommand;
+import org.bukkit.event.EventHandler;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin {
+
+    private SwappableEnchantsFactory swappableEnchantsFactory;
+    private PotionAurasFactory potionAurasFactory;
+    private StatTrackFactory statTrackFactory;
+
+    @Override
+    public void onEnable() {
+        /* Init mechanic factories */
+        swappableEnchantsFactory = new SwappableEnchantsFactory();
+        potionAurasFactory = new PotionAurasFactory();
+        statTrackFactory = new StatTrackFactory();
+
+        /* Registering */
+        registerListeners();
+        registerCommands();
+        hookIntoNexoMechanics();
+
+        /* Create runnable instances */
+        new PotionAurasTask(this, potionAurasFactory).start();
+    }
+
+    private void registerListeners() {
+        registerListener(new SwappableEnchantsListener(this, swappableEnchantsFactory));
+        registerListener(new StatTrackListener(this, statTrackFactory));
+    }
+
+    private void registerCommands() {
+        /* None Implemented */
+    }
+
+    private void hookIntoNexoMechanics() {
+        Logs.logInfo("MoreNexoMechanics → Waiting for Nexo registry event...");
+
+        registerListener(new Listener() {
+            @EventHandler
+            public void nRegister(NexoMechanicsRegisteredEvent event) {
+                MechanicsManager.INSTANCE.registerMechanicFactory(swappableEnchantsFactory, true);
+                MechanicsManager.INSTANCE.registerMechanicFactory(potionAurasFactory, true);
+                MechanicsManager.INSTANCE.registerMechanicFactory(statTrackFactory, true);
+
+                Logs.logInfo("Registered Swappable Enchants");
+                Logs.logInfo("Registered Potion Auras");
+                Logs.logInfo("Registered Stat Track");
+            }
+        });
+    }
+
+    private void registerListener(Listener listener) {
+        Bukkit.getPluginManager().registerEvents(listener, this);
+    }
+
+    private void registerCommand(String name, CommandExecutor executor) {
+        PluginCommand command = getCommand(name);
+        if (command == null) {
+            throw new IllegalStateException("Command '" + name + "' is not registered!");
+        }
+        command.setExecutor(executor);
+    }
+
+}
